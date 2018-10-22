7f5f8047bf498fabde6932028330e97c24deac7b
Dade Murphy <kona076@yahoo.com>
2018-10-22 15:40:51 -0700

    Add project files.

---
+++ SourceControl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SourceControl.Startup))]
+namespace SourceControl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
